b5aa2a3a68ec47d522b48937f2506ccdffffadb7
Anastasiia <nastya.rudyk@gmail.com>
2020-07-24 14:18:54 +0300

    bio script

---
+++ Assets/Scripts/World/Biome.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+[System.Serializable]
+public class Biome 
+{
+    public string name; 
+    public float waterLevel;
+    public float heightMultiplier; //Used only for the initial map generation
+    public AnimationCurve heightCurve; //Used only for the initial map generation
+    public List<TerrainType> terrainTypes;
+
+    public List<TerrainType> getTerrainTypes ()
+    {
+        return terrainTypes.OrderBy(e => e.height).ToList();
+    }
+}
