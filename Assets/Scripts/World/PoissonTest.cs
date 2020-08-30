20836059aaee7b782ca05041c166f7f5059dcf60
Anastasiia <nastya.rudyk@gmail.com>
2020-08-30 17:18:40 +0300

    random trees test

---
+++ Assets/Scripts/World/PoissonTest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PoissonTest : MonoBehaviour
+{
+
+    public float radius = 1;
+    public Vector2 regionSize; 
+    public int rejectAfterAttempts;
+    public float displayRadius;
+
+    private List<Vector2> points;
+
+    void OnValidate()
+    {
+        points = PoissonDiscSampling.GeneratePoints(radius, regionSize, rejectAfterAttempts);
+    }
+
+   
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireCube(regionSize / 2, regionSize); 
+        if (points != null)
+        {
+            foreach (var v in points)
+            {
+                Gizmos.DrawSphere(v, displayRadius);
+            }
+        }
+    }
+}
+
